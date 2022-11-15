1c2ddef25fc52d5c6760cb5065de8419fbbc890a
Athena Serene <aserenehoosang@gmail.com>
2022-11-15 02:30:21 -0800

    added food-index and food-show

--- server.js
+++ server.js
@@ -2,15 +2,36 @@ const express = require("express")
 const app = express()
 const PORT = 3000
 const drinks = require("./models/drinks")
+const food = require("./models/food")
 app.get("/", (req, res)=>{
-    res.send("Welcome to the Gitpub App!")
+    res.render("home.ejs")
 })
 app.get("/drinks", (req, res)=>{
     // res.send("Welocme to Drinks")
     res.render(
         "drinks-index.ejs",
-    {
-        allDrinks:drinks
+        {
+            allDrinks:drinks
+        })
+})
+app.get("/food", (req, res)=>{
+    res.render(
+        "food-index.ejs",
+        {
+            allFood: food
+        }
+    )
+})
+app.get("/drinks/:id", (req, res)=>{
+    res.render("drinks-show.ejs", {
+        drinks: drinks[req.params.id],
+        index: req.params.id
+    })
+})
+app.get("/food/:id", (req, res)=>{
+    res.render("food-show.ejs", {
+        food: food[req.params.id],
+        index: req.params.id
     })
 })
 app.listen(PORT, (req, res)=>{
