29427c944248773fdf15d3beac0085e0f385bc15
Athena Serene <aserenehoosang@gmail.com>
2022-11-15 01:43:13 -0800

    updated drinks-index.ejs

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
-const drinks = require("./drinks")
+const drinks = require("./models/drinks")
 app.get("/", (req, res)=>{
     res.send("Welcome to the Gitpub App!")
 })
 app.get("/drinks", (req, res)=>{
-    res.send(drinks)
+    // res.send("Welocme to Drinks")
+    res.render(
+        "drinks-index.ejs",
+    {
+        allDrinks:drinks
+    })
 })
 app.listen(PORT, (req, res)=>{
     console.log(`The server is listening from port ${PORT}...`)
