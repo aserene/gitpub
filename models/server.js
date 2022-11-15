b73c40c88f7022e176a35bee29121ef3416ba045
Athena Serene <aserenehoosang@gmail.com>
2022-11-15 00:37:59 -0800

    created /drinks route

--- models/server.js
+++ models/server.js
@@ -1,8 +1,12 @@
 const express = require("express")
 const app = express()
 const PORT = 3000
+const drinks = require("./drinks")
 app.get("/", (req, res)=>{
     res.send("Welcome to the Gitpub App!")
+})
+app.get("/drinks", (req, res)=>{
+    res.send(drinks)
 })
 app.listen(PORT, (req, res)=>{
     console.log(`The server is listening from port ${PORT}...`)
